6acb2845533c92cc28f5d34579e5c4b982039726
nangelsoto <122932482+nangelsoto@users.noreply.github.com>
2024-02-22 07:25:33 -0500

    Update intro.py

--- intro.py
+++ intro.py
@@ -2,3 +2,7 @@ import streamlit as st
 from PIL import Image
 
 st.title("Mi primera App!!")
+
+st.header("Este es el espacio para desarrollar las aplicaciones que considere de mi agrado para interfaces multimodales")
+st.write("facilmente puedo realizar backend y frontend")
+image= Image.open("Imagen1.png")
